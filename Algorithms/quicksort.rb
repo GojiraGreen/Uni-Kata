993edd6d0308809aca71addfb191a9191d57cc2a
GojiraGreen <gojira.green@gmail.com>
2014-05-05 23:45:26 +0200

    crapy quicksort

--- Algorithms/quicksort.rb
+++ Algorithms/quicksort.rb
@@ -1,1 +1,34 @@
+#! ruby
+
+def quicksort(tab, low, high)
+
+  if low >= high
+    return
+  end
+
+  min = low
+  max = high
+  div = tab[min]
+
+  while min < max
+    while div <= tab[max] and min < max
+      max -= 1
+    end
+    tab[min] = tab[max]
+    min += 1 and min < max
+    while div >= tab[min] and min < max
+      min += 1
+    end
+    tab[max] = tab[min]
+    max -= 1
+  end
+
+  tab[min] = div
+  quicksort(tab, low, min - 1)
+  quicksort(tab, min + 1, high)
+  tab
+end
+
+tab = [31,5,7,65,4,2,435,34,87,32,83]
+p quicksort(tab, 0, tab.length - 1)
 
