831a2d433a3ca93ae792220a72271cda902ab3bf
GojiraGreen <gojira.green@gmail.com>
2014-05-06 15:28:08 +0200

    quicksort done

--- Algorithms/quicksort.rb
+++ Algorithms/quicksort.rb
@@ -3,32 +3,45 @@
 def quicksort(tab, low, high)
 
   if low >= high
-    return
+    return tab
   end
 
   min = low
   max = high
   div = tab[min]
 
-  while min < max
-    while div <= tab[max] and min < max
+  while true
+    while min < max and div <= tab[max]
       max -= 1
     end
+    break if min >= max
     tab[min] = tab[max]
-    min += 1 and min < max
-    while div >= tab[min] and min < max
+    min += 1
+    while min < max and div >= tab[min]
       min += 1
     end
+    break if min >= max
     tab[max] = tab[min]
     max -= 1
   end
 
-  tab[min] = div
-  quicksort(tab, low, min - 1)
-  quicksort(tab, min + 1, high)
+  tab[max] = div
+  quicksort(tab, low, max - 1)
+  quicksort(tab, max + 1, high)
   tab
 end
 
-tab = [31,5,7,65,4,2,435,34,87,32,83]
-p quicksort(tab, 0, tab.length - 1)
+module Enumerable
+  def sorted?
+    each_cons(2).all? {|a, b| (a <=> b) <= 0 }
+  end
+end
+
+num = Random.new
+tab = Array.new(10) {|n| n = num.rand(0..100)}
+
+quicksort(tab, 0, tab.length - 1)
+
+print "sorted? #{tab.sorted?}\n"
+print tab
 
